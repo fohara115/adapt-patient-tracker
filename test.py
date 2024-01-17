cacfb5979a1e433a1179184ad6e71ddbfa8e3d59
Finn O'Hara <finn.ohara1155@gmail.com>
2024-01-16 19:10:34 -0500

    refactor displaying, started investigating loading format

---
+++ test.py
@@ -0,0 +1,11 @@
+from utils import display_pipeline
+from load import load_bag_file, load_live_stream
+
+# Test feed
+pipeline, config = load_live_stream()
+display_pipeline(pipeline, config)
+
+# Test recording
+filename = "data/20231201_152820.bag"
+pipeline, config = load_bag_file()
+display_pipeline(pipeline, config)
