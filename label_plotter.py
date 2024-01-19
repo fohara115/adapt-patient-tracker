e2ee6fc9069fd11af67bc7054847747120338f16
Finn O'Hara <finn.ohara1155@gmail.com>
2024-01-19 10:50:29 -0500

    updated plotter script

--- label_plotter.py
+++ label_plotter.py
@@ -2,24 +2,27 @@ import cv2
 import numpy as np
 import os
 from tqdm import tqdm
+import pandas as pd
 
 # Define the input folder containing PNG frames and the input CSV file
-frame_folder = 'frames/20231124_163447/'  # Replace with your frame folder path
-csv_file = 'labels/20231124_163447_label1.txt'     # Replace with your CSV file path
+frame_folder = 'frames/20231201_152420/'  # Replace with your frame folder path
+csv_file = 'proc_labels/20231201_152420.csv'     # Replace with your CSV file path
 
 # Output video parameters
-output_video_path = 'output_video_420.mp4'
+output_video_path = 'label_20231201_152420.mp4'
 frame_width, frame_height = 640, 480  # Adjust as needed
 frame_rate = 30
 
 # Function to read coordinates from CSV file
 def read_coordinates_from_csv(file_path):
     coordinates = {}
-    with open(file_path, 'r') as f:
-        for line in f:
-            frame_idx, x, y = map(int, line.strip().split(','))
-            coordinates[frame_idx] = (x, y)
-    return coordinates
+
+    df = pd.read_csv(file_path)
+    # with open(file_path, 'r') as f:
+    #     for line in f:
+    #         frame_idx, x, y = map(int, line.strip().split(','))
+    #         coordinates[frame_idx] = (x, y)
+    return df
 
 # Function to add a circle to a frame
 def add_circle_to_frame(frame, center, radius=25):
@@ -39,8 +42,8 @@ for frame_idx in tqdm(range(len(os.listdir(frame_folder)))):
     if frame is None:
         continue
     
-    if frame_idx in coordinates:
-        circle_center = coordinates[frame_idx]
+    if frame_idx in coordinates['idx']:
+        circle_center = (int(coordinates['x_coord'][frame_idx]), int(coordinates['y_coord'][frame_idx]))
         frame = add_circle_to_frame(frame, circle_center)
     
     frame = cv2.resize(frame, (frame_width, frame_height))
