4febcdc530058d1bd281c4fb7cf0430063bb1c0f
fohara115 <finn.ohara1155@gmail.com>
2024-02-02 16:53:42 -0500

    added output file logging, only run through prerecordings once

--- main.py
+++ main.py
@@ -2,7 +2,6 @@ import pyrealsense2 as rs
 import numpy as np
 import cv2
 import yaml
-print(np.__version__)
 
 from load import load_bag_file, load_live_stream
 
@@ -27,6 +26,8 @@ distance_trigger = cfg['tracker']['distance_trigger']
 disp = cfg['runtime']['display_window']
 print_dist = cfg['runtime']['print_distance']
 print_coord = cfg['runtime']['print_coord']
+write_output = cfg['runtime']['write_output']
+output_dir = cfg['runtime']['output_root']+cfg['runtime']['output_filename']
 
 
 
@@ -45,6 +46,7 @@ depth_scale = depth_sensor.get_depth_scale()
 clipping_distance = clip_limit / depth_scale
 align_to = rs.stream.color
 align = rs.align(align_to)
+t_prev = 0
 
 # Setup tracker
 init_bbox = (image_width//2 - (bbox_width//2), image_height//2 - (bbox_height//2), bbox_height, bbox_width)
@@ -53,6 +55,11 @@ if tracker_type == 'MIL':
     tracker = cv2.TrackerMIL_create()
 else:
     raise Exception('Tracker type us bit supported.')
+
+# Setup output
+if write_output:
+    with open(output_dir, 'w') as o:
+        o.write(f"OUTPUT for LIVE: {live_input}   INPUT: {filename}\n")
 
 
 
@@ -69,6 +76,8 @@ while True:
         continue
     depth_image = np.asanyarray(aligned_depth_frame.get_data())
     color_image = np.asanyarray(color_frame.get_data())
+    t = frames.get_timestamp()
+
 
     # Remove background and render images
     depth_image_3d = np.dstack((depth_image,depth_image,depth_image)) #depth image is 1 channel, color is 3 channels
@@ -99,7 +108,7 @@ while True:
         if print_dist:
             print(bbox_min_dist)
         if print_coord:
-            print('dummy')
+            print(f"Centre: ({int(bbox[0] + bbox[2]//2)}, {int(bbox[1] + bbox[3]//2)})")
     
     if disp:
         #cv2.namedWindow('ADAPT Patient Tracker', cv2.WINDOW_NORMAL)
@@ -109,5 +118,18 @@ while True:
         if key & 0xFF == ord('q') or key == 27:
             cv2.destroyAllWindows()
             break
+
+    if write_output:
+        with open(output_dir, "a") as f:
+            if (tracker_init and ret):
+                print(f"{t},{int(bbox[0] + bbox[2]//2)},{int(bbox[1] + bbox[3]//2)},{bbox_min_dist}", file=f)
+            else:
+                print(f"{t},None,None,{center_dist}", file=f)
+
+    # Check for end of recorded tape 
+    if (not live_input):
+        if (t < t_prev):
+            break
+        t_prev = t
 
 pipeline.stop()
