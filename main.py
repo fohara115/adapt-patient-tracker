b8f0038c6f7dbf3bcc73a6b5beddaf1d4da8be84
fohara115 <finn.ohara1155@gmail.com>
2024-02-07 11:55:47 -0500

    pre serial work

--- main.py
+++ main.py
@@ -67,8 +67,8 @@ if tracker_type == 'MIL':
     tracker = cv2.TrackerMIL_create()
 elif tracker_type == 'BOOSTING':
     tracker = cv2.TrackerBoosting_create()
-elif tracker_type == 'KCF':
-    tracker = cv2.TrackerKCF_create()
+elif tracker_type == 'CSRT':
+    tracker = cv2.TrackerCSRT_create()
 elif tracker_type == 'TLD':
     tracker = cv2.TrackerTLD_create()
 elif tracker_type == 'MEDIANFLOW':
