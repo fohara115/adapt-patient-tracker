f4dacb860757f1391b7533880f72157ad7223dd6
fohara115 <finn.ohara1155@gmail.com>
2024-03-17 14:38:30 -0400

    removed mdl file and added batch file

---
+++ run.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+python3 main.py 20240210_113406.bag M17_C1_
+python3 main.py 20240210_113621.bag M17_C1_
+python3 main.py 20240210_113801.bag M17_C1_
+python3 main.py 20240210_113928.bag M17_C1_
+python3 main.py 20240210_114046.bag M17_C1_
+python3 main.py 20240210_114325.bag M17_C1_
+python3 main.py 20240210_114433.bag M17_C1_
+python3 main.py 20240210_114506.bag M17_C1_
+python3 main.py 20240316_185039.bag M17_C1_
+python3 main.py 20240316_185126.bag M17_C1_
+python3 main.py 20240316_185606.bag M17_C1_
+python3 main.py 20240316_185653.bag M17_C1_
+
